1cdbdc33cf4e6c1ac6ccd253f02d6ef802b24da5
Ruan <ruanelpidio@hotmail.com>
2021-11-11 09:23:48 -0300

    update(): Added CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./src/routes");
 const app = express();
 const port = process.env.PORT || 3000
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
