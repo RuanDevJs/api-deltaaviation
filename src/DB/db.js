358b2b21aa8d3fd27f2d0be530ab5c7018bfddaf
Ruan <ruanelpidio@hotmail.com>
2021-11-10 21:48:56 -0300

    fix

--- src/DB/db.js
+++ src/DB/db.js
@@ -1,6 +1,6 @@
 const mongo = require("mongoose");
 
-mongo.connect("mongodb+srv://ruan:V99cvirzdD4AqUTB@cluster0.i3z0l.mongodb.net/myFirstDatabase?retryWrites=true")
+mongo.connect(process.env.DB_URL)
 .then(res => {
     console.debug("DB", "Connected");
 })
